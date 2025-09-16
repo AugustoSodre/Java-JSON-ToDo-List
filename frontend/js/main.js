e918405899bbc6b125e9311f511c007c5813f905
AugustoSodre <augustolima73@yahoo.com>
2025-09-16 20:37:06 -0300

    feat: completed Create Task form, implemented localstorage Create (C) operation

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -19,19 +19,36 @@ if(!localStorage.getItem("task_list")){
 
 task_list = JSON.parse(localStorage.getItem("task_list"))
 
-console.log(task_list[0])
+console.log(document.getElementById("form_new_priority").options)
 
 
 //Handling the Create Task form
-document.getElementById("new_task_form").
-addEventListener("submit", function (){
-    
-    let name = document.getElementById("form_new_name").value;
+document.getElementById("new_task_form").addEventListener("submit", function (){
+
+    function getCurrentPriority(){
+        let array_options = document.getElementById("form_new_priority").options
+        for(i = 0; i < array_options.length; i++){
+            if(array_options[i].selected){
+                return array_options[i].value
+            }
+        }
+    }
+
+    function getCurrentStatus(){
+        let array_options = document.getElementById("form_new_status").options
+        for(i = 0; i < array_options.length; i++){
+            if(array_options[i].selected){
+                return array_options[i].value
+            }
+        }
+    }
+
+    let name        = document.getElementById("form_new_name").value;
     let description = document.getElementById("form_new_description").value;
-    let deadline = document.getElementById("form_new_date").value;
-    let priority
-    let category = document.getElementById("form_new_name").value;
-    let task_status
+    let deadline    = document.getElementById("form_new_date").value;
+    let priority    = getCurrentPriority()
+    let category    = document.getElementById("form_new_name").value;
+    let task_status = getCurrentStatus()
 
     const new_task = new Task(name, description, deadline,
          priority, category, task_status)
