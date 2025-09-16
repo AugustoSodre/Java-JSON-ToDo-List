d8476103f458335f25f33160f795f213a114894b
AugustoSodre <augustolima73@yahoo.com>
2025-09-16 12:57:39 -0300

    feat: form that creates tasks and saves them in the local storage. TODO: get priority and get status from the form to save them as well (pending).

---
+++ frontend/js/main.js
@@ -0,0 +1,44 @@
+//Defining Task Class
+class Task{
+    constructor(name, description, deadline, priority, category, task_status) {
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.category = category;
+        this.task_status = task_status;
+    }
+}
+
+//Defining Task List and its methods to save and retrieve items
+let task_list = []
+
+if(!localStorage.getItem("task_list")){
+    localStorage.setItem("task_list" , JSON.stringify([]))
+}
+
+task_list = JSON.parse(localStorage.getItem("task_list"))
+
+console.log(task_list[0])
+
+
+//Handling the Create Task form
+document.getElementById("new_task_form").
+addEventListener("submit", function (){
+    
+    let name = document.getElementById("form_new_name").value;
+    let description = document.getElementById("form_new_description").value;
+    let deadline = document.getElementById("form_new_date").value;
+    let priority
+    let category = document.getElementById("form_new_name").value;
+    let task_status
+
+    const new_task = new Task(name, description, deadline,
+         priority, category, task_status)
+
+    task_list.push(new_task)
+    
+    localStorage.setItem("task_list" , JSON.stringify(task_list))
+})
+
+
