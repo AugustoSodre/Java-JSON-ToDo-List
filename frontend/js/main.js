9c976c42ebd485843057960fe473eb9822c6a020
AugustoSodre <augustolima73@yahoo.com>
2025-09-16 22:19:17 -0300

    feat: filtered the listing of TODO, DOING and DONE tasks into different lists. Started developing the Frontend with new card-style to-do tasks

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -22,7 +22,7 @@ task_list = JSON.parse(localStorage.getItem("task_list"))
 console.log(document.getElementById("form_new_priority").options)
 
 
-//Handling the Create Task form
+//Handling the Create Task form - Create (C) operation
 document.getElementById("new_task_form").addEventListener("submit", function (){
 
     function getCurrentPriority(){
@@ -50,12 +50,56 @@ document.getElementById("new_task_form").addEventListener("submit", function (){
     let category    = document.getElementById("form_new_name").value;
     let task_status = getCurrentStatus()
 
+    //Creating a new task assigning respective items to the constructor
     const new_task = new Task(name, description, deadline,
          priority, category, task_status)
 
+
+    //Pushing the new created task to the task list
     task_list.push(new_task)
     
+    //Saving the new item in the localstorage
     localStorage.setItem("task_list" , JSON.stringify(task_list))
 })
 
+//Organizing the tasks into lists - Read (R) operation
+let todo_list = []
+let doing_list = []
+let done_list = []
+for(i = 0; i < task_list.length; i++){
+    switch(task_list[i].task_status){
+        case "TODO":
+            todo_list.push(task_list[i])
+            break
+        case "DOING":
+            doing_list.push(task_list[i])
+            break
 
+        case "DONE":
+            done_list.push(task_list[i])
+            break
+        default:
+            console.log("Invalid task" + task_list[i])
+            break
+    }
+}
+
+console.log(todo_list)
+console.log(doing_list)
+console.log(done_list)
+
+//Listing TODO tasks
+let todo_HTML = ""
+
+
+
+
+// document.getElementById("todo_list").innerHTML = todo_HTML
+
+//Listing DOING tasks
+
+//Listing DONE tasks
+
+//Updating tasks - Update (U) operation
+
+//Deleting tasks - Delete (D) operation
